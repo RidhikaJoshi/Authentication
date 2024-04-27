e90473c83025236b465f1280c1a560040063beba
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-04-27 16:52:37 +0530

    Add signup route and email helper function

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,29 +1,47 @@
+import User from '@/models/user.model';
 import nodemailer from 'nodemailer';
+import bcryptjs from 'bcryptjs';
 
 export const sendEmail=async({email,emailType,userId}:any)=>
 {
     try{
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
+        const hashedToken=await bcryptjs.hash(userId.toString(),10);
+        if(emailType==='VERIFY')
+        {
+            await User.findByIdAndUpdate(userId,{
+                verifyToken:hashedToken,
+                verifyTokenExpiry:Date.now()+3600000
+            });
+        }
+        else if(emailType==='RESET')
+            {
+                await User.findByIdAndUpdate(userId,{
+                    forgotPasswordToken:hashedToken,
+                    forgotPasswordTokenExpiry:Date.now()+3600000
+                }
+                );
+            }
+
+       var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
             auth: {
-                user: "maddison53@ethereal.email",
-                pass: "jn7jnAPss4f63QBp6D",
-            },
+                user: process.env.NODEMAILER_USER,
+                pass: process.env.NODEMAILER_PASS,
+            }
             });
             
         const mailOptions={
             from: 'cherrysmrh@gmail.com', // sender address
             to:email, //  receiver
             subject: emailType==='VERIFY' ? 'Verify your Email' : 'Reset your Password', // Subject line
-            html: "<b>Hello world?</b>", // html body
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyEmail?token=${hashedToken}">here</a> to ${emailType==='VERIFY' ? 'verify your email' : 'Reset your password'}</p>`
         }
 
-        const mailResponse=await transporter.sendMail(mailOptions);
+        const mailResponse=await transport.sendMail(mailOptions);
         return mailResponse;
      }catch(error:any)
         {
-                throw new Error(error);
+                throw new Error(error.message);
         }
 }
